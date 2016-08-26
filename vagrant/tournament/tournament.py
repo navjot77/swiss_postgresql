b142a51c909652333d25e49ce10da73f7c2092f2
navjot <mail.navjot77@gmail.com>
2016-08-26 11:00:21 -0700

    Identation and view query change

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,11 +13,12 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    db=connect()
-    cur=db.cursor()
+    db = connect()
+    cur = db.cursor()
     cur.execute("delete from tournament01.player_matches;")
     db.commit()
     db.close()
+
 
 def deletePlayers():
     """Remove all the player records from the database."""
@@ -27,37 +28,38 @@ def deletePlayers():
     db.commit()
     db.close()
 
+
 def countPlayers():
     """Returns the number of players currently registered."""
     db = connect()
     cur = db.cursor()
     cur.execute("select count(id) from tournament01.player_records;")
-    players_count=cur.fetchall()[0][0]
+    # [0][0] is meant to fetch the right column in tuple
+    players_count = cur.fetchall()[0][0]
     db.close()
-    print "========================"
-    print players_count
     return players_count
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
     db = connect()
     cur = db.cursor()
-    cur.execute("insert into tournament01.player_records (name) VALUES (%s);",(name,))
+    cur.execute(
+        "insert into tournament01.player_records (name) VALUES (%s);", (name,))
     db.commit()
-    cur.execute("select * from tournament01.player_records ")
     db.close()
+
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
-    The first entry in the list should be the player in first place, or a player
+    The first entry in the list should be the player in first place,or a player
     tied for first place if there is currently a tie.
 
     Returns:
@@ -69,10 +71,14 @@ def playerStandings():
     """
     db = connect()
     cur = db.cursor()
-    cur.execute("select id, name, wins, matches from players_standing ORDER BY wins DESC, matches ASC ")
+    # Players_standing is a view.
+    cur.execute(
+    "select id, name, wins, matches from players_standing ORDER BY wins DESC")
     result = cur.fetchall()
     db.close()
+    # returning a list
     return result
+
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -82,20 +88,21 @@ def reportMatch(winner, loser):
       loser:  the id number of the player who lost
     """
     db = connect()
-    c = db.cursor()
-    c.execute("insert into tournament01.player_matches values (%s, %s)", (winner, loser))
+    cur = db.cursor()
+    cur.execute(
+    "insert into tournament01.player_matches values (%s, %s)", (winner, loser))
     db.commit()
     db.close()
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -103,11 +110,15 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    players_standing=playerStandings()
+    # Get the logical table player standing in a list
+    players_standing = playerStandings()
     list_players = []
     while len(players_standing) > 1:
+        # pop will take  first element from list, here it will be row of player
+        #  with higher standing
         player1 = players_standing.pop(0)
         player2 = players_standing.pop(0)
+        # create a tupple and append it in list
         players = (player1[0], player1[1], player2[0], player2[1])
         list_players.append(players)
     return list_players
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,8 +5,10 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+-- Drop the database tournament initially
 drop database tournament;
 CREATE DATABASE tournament;
+-- connect to database, this is command line instruction
 \c tournament;
 
 CREATE SCHEMA tournament01;
@@ -22,15 +24,17 @@ looser_id INT REFERENCES tournament01.player_records(id)
 );
 
 CREATE VIEW players_standing AS
- SELECT
+  SELECT
   id,
   name,
-  count(tournament01.player_matches.winner_id) as wins,
- (select count(*) from tournament01.player_matches where id=tournament01.player_matches.winner_id or
-    id=tournament01.player_matches.looser_id) as matches
-   from tournament01.player_records left join tournament01.player_matches
-    on tournament01.player_records.id = tournament01.player_matches.winner_id
-      group by tournament01.player_records.id;
+  (SELECT count(tournament01.player_matches.winner_id)
+  from tournament01.player_matches
+  GROUP BY tournament01.player_matches.winner_id) as wins,
+  (select count(*) from tournament01.player_matches where
+  id=tournament01.player_matches.winner_id or
+  id=tournament01.player_matches.looser_id) as matches
+  from tournament01.player_records left join tournament01.player_matches
+  on tournament01.player_records.id = tournament01.player_matches.winner_id;
 
 
 
